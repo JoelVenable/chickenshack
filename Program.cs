a369263b04743a8f94ece1217c05fd340c2c0653
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-11 08:18:26 -0500

    Added try/catch block

--- Program.cs
+++ Program.cs
@@ -2,31 +2,43 @@
 using System.Collections.Generic;
 namespace chickenshack
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          Company chickenShack = new Company() {
-              Name= "Greasy Pete's Chicken Shack"
-          };
-            chickenShack.AddEmployee(new Employee() {
-                FirstName = "Pete",
-                LastName = "Shackleton"
-            });
-             chickenShack.AddEmployee(new Employee() {
-                FirstName = "Molly",
-                LastName = "Frycook"
-            });
-             chickenShack.AddEmployee(new Employee() {
-                FirstName = "Pat",
-                LastName = "Buttersmith"
-            });
+      Company chickenShack = new Company()
+      {
+        Name = "Greasy Pete's Chicken Shack"
+      };
+      chickenShack.AddEmployee(new Employee()
+      {
+        FirstName = "Pete",
+        LastName = "Shackleton"
+      });
+      chickenShack.AddEmployee(new Employee()
+      {
+        FirstName = "Molly",
+        LastName = "Frycook"
+      });
+      chickenShack.AddEmployee(new Employee()
+      {
+        FirstName = "Pat",
+        LastName = "Buttersmith"
+      });
 
-            List<int> employeeIds = new List<int>() { 0, 11, 2} ;
-            employeeIds.ForEach(id => {
-                Employee employee = chickenShack.GetEmployeeById(id);
-                Console.WriteLine($"Employee #{id} is {employee.FirstName} {employee.LastName}");
-            });
-        }
+      List<int> employeeIds = new List<int>() { 0, 11, 2 };
+      try
+      {
+        employeeIds.ForEach(id =>
+        {
+          Employee employee = chickenShack.GetEmployeeById(id);
+          Console.WriteLine($"Employee #{id} is {employee.FirstName} {employee.LastName}");
+        });
+      }
+      catch (ArgumentOutOfRangeException ex)
+      {
+        Console.WriteLine($"Something went wrong: {ex.Message}");
+      }
     }
+  }
 }
